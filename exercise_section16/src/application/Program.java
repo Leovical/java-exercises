3ab3e38caf5ffe6ff668efdc37350e81e649c41d
Leonardo Victor <leovictor.alves@hotmail.com>
2025-08-01 17:59:06 -0300

    creating the main skeleton - reading the first variables of the contract

---
+++ exercise_section16/src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Contract;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Enter with the contract data:");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Date (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, date, totalValue);
+		
+		
+		
+		sc.close();
+	}
+
+}
