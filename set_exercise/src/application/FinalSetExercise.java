dfd3def4a9e5ad6bb395d1a062092236ea8b07ba
Leonardo Victor <leovictor.alves@hotmail.com>
2025-08-25 22:38:46 -0300

    another exercise about Set - basic application

---
+++ set_exercise/src/application/FinalSetExercise.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class FinalSetExercise {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
+		
+	
+	}
+
+}
