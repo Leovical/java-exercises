d6b6cc89379ad64b5c3eaa7f4ae202fdcf471cd1
Leonardo Victor <leovictor.alves@hotmail.com>
2025-08-25 22:48:18 -0300

    exercise finished - Set

--- set_exercise/src/application/FinalSetExercise.java
+++ set_exercise/src/application/FinalSetExercise.java
@@ -14,7 +14,36 @@ public class FinalSetExercise {
 		Set<Integer> b = new HashSet<>();
 		Set<Integer> c = new HashSet<>();
 		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
+		}
+		
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int number = sc.nextInt();
+			b.add(number);
+		}
+		
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int number = sc.nextInt();
+			c.add(number);
+		}
+		
+		Set<Integer> total = new HashSet<>();
+		total.addAll(a);
+		total.addAll(b);
+		total.addAll(c);
+		
+		System.out.println();
+		System.out.println("Total students: " + total.size());
 	
+		sc.close();
 	}
 
 }
